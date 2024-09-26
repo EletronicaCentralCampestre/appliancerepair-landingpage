0630d18ee69252d6e6c0824cca7d41f94567d4f3
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-09-26 14:08:07 -0300

    Update package.json and package-lock.json to include react-icons dependency

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Logo from "../assets/eletronicacentral-logo.png";
-import { Linkedin, Youtube, Facebook} from "lucide-react";
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
     const copyInput = () => {
@@ -10,20 +13,19 @@ const Footer = () => {
         alert("Link copiado: " + copyText);
       }
   return (
-    <div className="footer-wrapper container mx-8 py-24 flex" id="contato">
-        <div className="footer-section-one justify-center">
-            <div className="footer-logo-container max-w-xs">
+    <div className="footer-wrapper  pt-24 flex" id="contato">
+        <div className="footer-section-one  ">
+            <div className="footer-logo-container ml-8 max-w-[160px]  ">
                 <img src={Logo} alt="logo" className="w-24" />
             </div>
-            <div className="footer-icons mt-5 ">
-                <a href="/" className=''><Youtube size={28}/></a>
-                <a href="/" className='mr-4 h-6 w-6'><Facebook size={28} /></a>
-                <a href="/" className='mr-4 h-6 w-6'><Linkedin size={28}/></a>
-                <a href="/" className='mr-4 h-6 w-6'><Linkedin size={28}/></a>
+            <div className="footer-icons mt-5  ml-3 flex">
+                <a href="/" className='mt-8 mr-4 text-secondary'><FaYoutube size={32} /></a>
+                <a href="/" className='mt-8 mr-4 text-secondary'><FaFacebook size={32} /></a>
+                <a href="/" className='mt-8 mr-4 text-secondary'><FaInstagram size={32} /></a>
             </div>
         </div>
     <div className="footer-section-two flex-1 flex justify-end">
-      <div className="footer-section-columns flex flex-col min-w-[190px]">
+      <div className="footer-section-columns  flex flex-col min-w-[190px]">
         <span className='mx-2 my-1 font-semibold text-gray-500 cursor-pointer'>Qualidade</span>
         <span className='mx-2 my-1 font-semibold text-gray-500 cursor-pointer'>Ajuda</span>
         <span className='mx-2 my-1 font-semibold text-gray-500 cursor-pointer' onClick={copyInput}>Compartilhar</span>
